e67faa108f50f33135c0ac56dbef2a3891f3d11e
d2vicky <vicky_beauty@qq.com>
2016-09-06 20:10:40 -0400

    Adding test case of 1 Shuang's back-end

--- test/cs4321/project1/BuildPrefixExpressionTreeVisitorTest.java
+++ test/cs4321/project1/BuildPrefixExpressionTreeVisitorTest.java
@@ -186,5 +186,36 @@ public class BuildPrefixExpressionTreeVisitorTest {
 		assertTrue(result instanceof NumberListNode);
 		assertEquals(((NumberListNode) result).getData(), 1.0, DELTA);
 	}
+	
+	/**
+	 * Shuang Zhang sz468
+	 */
+	@Test
+	public void testCompositeNodes() {
+		TreeNode n1 = new LeafTreeNode(1.0);
+		TreeNode n2 = new LeafTreeNode(3.0);
+		TreeNode n3 = new AdditionTreeNode(n2, n1);
+		TreeNode n4 = new LeafTreeNode(2.0);
+		TreeNode n5 = new UnaryMinusTreeNode(n4);
+		TreeNode n6 = new MultiplicationTreeNode(n5, n3);
+		
+        BuildPrefixExpressionTreeVisitor v1 = new BuildPrefixExpressionTreeVisitor();
+		n6.accept(v1);
+		ListNode result = v1.getResult(); // *
+		assertTrue(result instanceof MultiplicationListNode);
+		result = result.getNext(); // ~
+		assertTrue(result instanceof UnaryMinusListNode);
+		result = result.getNext(); // 2.0
+		assertTrue(result instanceof NumberListNode);
+		assertEquals(((NumberListNode) result).getData(), 2.0, DELTA);
+		result = result.getNext(); // +
+		assertTrue(result instanceof AdditionListNode);
+		result = result.getNext();// 3.0
+		assertTrue(result instanceof NumberListNode);
+		assertEquals(((NumberListNode) result).getData(), 3.0, DELTA);
+		result = result.getNext();// 1.0
+		assertTrue(result instanceof NumberListNode);
+		assertEquals(((NumberListNode) result).getData(), 1.0, DELTA);
+	}
 
 }
