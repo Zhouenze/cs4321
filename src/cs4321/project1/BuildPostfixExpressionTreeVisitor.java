ace32bee68e7736b43aedb3cc627b4be38100aef
Weicheng Yu <weicheny1993@gmail.com>
2016-09-08 20:08:34 -0400

    All comments added

--- src/cs4321/project1/BuildPostfixExpressionTreeVisitor.java
+++ src/cs4321/project1/BuildPostfixExpressionTreeVisitor.java
@@ -4,27 +4,45 @@ import cs4321.project1.list.*;
 import cs4321.project1.tree.*;
 
 /**
- * Provide a comment about what your class does and the overall logic
- * 
- * @author Your names and netids go here
+ * There are two variables head and current that I use to keep building
+ * the tree. The head is used to return a Linked List representation. 
+ * The current ListNode is used to keep building the tail of the linked list
+ * The general algorithm is to build the current node recursively based on 
+ * its type. 
+ * @author Weicheng Yu wy248
  */
 public class BuildPostfixExpressionTreeVisitor implements TreeVisitor {
 
-	//dummy head node head
-	private ListNode head;
-	private ListNode current;
+	
+	private ListNode head; // dummy head node head
+	private ListNode current; // tail node
 
+	/**
+	* Constructor that initializes head and current variables
+	*
+	*
+	*/
 	public BuildPostfixExpressionTreeVisitor() {
-		// TODO fill me in
-		head = new NumberListNode(-1);
+		
+		head = new NumberListNode(-1); // dummy head
 		current = head;
 	}
 
+	/**
+	* Method to get the Linked List representation
+	*
+	*@return head of the Linked List
+	*/
 	public ListNode getResult() {
-		// TODO fill me in
 		return head.getNext();
 	}
 
+	/**
+	* Method to update current based on LeafTreeNode
+	*
+	* @param node
+	*			 the node to be visited
+	*/	
 	@Override
 	public void visit(LeafTreeNode node) {
 		current.setNext(new NumberListNode(node.getData()));
@@ -32,6 +50,12 @@ public class BuildPostfixExpressionTreeVisitor implements TreeVisitor {
 		
 	}
 
+	/**
+	* Method to update current based on UnaryMinusTreeNode
+	*
+	* @param node
+	*			 the node to be visited
+	*/	
 	@Override
 	public void visit(UnaryMinusTreeNode node) {
 		node.getChild().accept(this);
@@ -39,6 +63,12 @@ public class BuildPostfixExpressionTreeVisitor implements TreeVisitor {
 		current = current.getNext();
 	}
 
+	/**
+	* Method to update current based on AdditionTreeNode
+	*
+	* @param node
+	*			 the node to be visited
+	*/	
 	@Override
 	public void visit(AdditionTreeNode node) {
 		node.getLeftChild().accept(this);
@@ -47,6 +77,12 @@ public class BuildPostfixExpressionTreeVisitor implements TreeVisitor {
 		current = current.getNext();
 	}
 
+	/**
+	* Method to update current based on MultiplicationTreeNode
+	*
+	* @param node
+	*			 the node to be visited
+	*/	
 	@Override
 	public void visit(MultiplicationTreeNode node) {
 		node.getLeftChild().accept(this);
@@ -55,6 +91,12 @@ public class BuildPostfixExpressionTreeVisitor implements TreeVisitor {
 		current = current.getNext();
 	}
 
+	/**
+	* Method to update current based on SubtractionTreeNode
+	*
+	* @param node
+	*			 the node to be visited
+	*/	
 	@Override
 	public void visit(SubtractionTreeNode node) {
 	
@@ -64,6 +106,12 @@ public class BuildPostfixExpressionTreeVisitor implements TreeVisitor {
 		current = current.getNext();
 	}
 
+	/**
+	* Method to update current based on DivisionTreeNode
+	*
+	* @param node
+	*			 the node to be visited
+	*/			 
 	@Override
 	public void visit(DivisionTreeNode node) {
 		node.getLeftChild().accept(this);
